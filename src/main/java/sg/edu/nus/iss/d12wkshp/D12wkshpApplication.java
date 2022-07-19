eb0ee2a45b9b96025094c59552ea98ceaa296f82
Irfan <irfanimanabub@gmail.com>
2022-07-19 09:56:20 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d12wkshp/D12wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D12wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D12wkshpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d12wkshp/D12wkshpApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12wkshp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D12wkshpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
