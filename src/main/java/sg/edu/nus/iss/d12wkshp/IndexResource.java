40e15c82f164ff40bfe8440f991d7dfedd12be52
Irfan <irfanimanabub@gmail.com>
2022-07-19 11:32:09 +0800

    completed lecture slide 17

--- src/main/java/sg/edu/nus/iss/d12wkshp/IndexResource.java
+++ src/main/java/sg/edu/nus/iss/d12wkshp/IndexResource.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Controller
 @RequestMapping(path = {"/indexresource", "/indexresource.html"})
 public class IndexResource {
     
     @GetMapping(produces = {"text/html"})
     public String index(Model model) {
+        model.addAttribute("currTime", (new Date()).toString());
+
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
+        
         return "indexresource";
     }
 
